8d737a86b374ffc1d3e44d93a8556a80d1c91a44
Arockiaraj Savarinathan <arockiarajs24@gmail.com>
2020-02-25 16:51:57 +0800

    initial upload

---
+++ src/test/java/com/sbt/discoveryserver/SbtDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbt.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbtDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
